490c07248d507b332cad0ddd50b5dde81a8a98dd
nayanaith@gmail.com <nayanaith@gmail.com>
2016-05-07 11:24:03 -0500

    Initial commit

---
+++ src/main/java/com/usingTestNg/App.java
@@ -0,0 +1,13 @@
+package com.usingTestNg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
